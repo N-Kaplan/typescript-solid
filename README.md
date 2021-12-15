2bc31b60d37507d2302a7d8e2d509f23bee4aa81
Naama Kaplan <nkaplany@gmail.com>
2021-12-15 10:41:47 +0100

    ex3 (Liskov principle) complete

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # typescript-solid
-introduction to TypeScript
+Introduction to TypeScript.
+
+SOLID principles:
+
+0. Single Responsibility principle : only changes to a single part of the specifications should be able to affect the class' specifications. (no 'God objects' which know and do everything.).
+1. Open-Closed principle: classes should be open for extension, closed for modification.
+    Dependencies: pass 1 object to another as a parameter.<br>
+    BLACKBOX principle: it should be possible to guess a function's purpose through its signature.
+2. Liskov principle: objects in a program should be replaceable with instances of their subtypes. [further reading](https://reflectoring.io/lsp-explained/).<br>
+   POLYMORPHISM: 2 different classes using the same interface are functionally interchangeable.<br>
+   Design by contract: assumes all client components that invoke an operation on a server component will meet the preconditions specified as required for that operation.<br>
--- SOLID/2.L/new.ts
+++ SOLID/2.L/new.ts
@@ -1,19 +1,153 @@
 //instruction: refactor the Discount class in 3 different classes with the same interface "VariableDiscount" & "FixedDiscount" & "NoDiscount"
+// further reading: https://reflectoring.io/lsp-explained/
 
 //This is called a Union, the discountType can only contain the following 2 values:
 type discountType = "variable" | "fixed" | "none";
 
-class VariableDiscount {
-    //is this equivalent to a constant?
-    private readonly _type: discountType = 'variable';
-    private readonly _value: number = 0;
+interface DiscountInterface {
+    //calculate price
+    apply(price: number):number;
+    //display calculation
+    showCalculation(price: number) : string;
+}
 
-    constructor(type: discountType, value: number) {
-        this._type = type;
+class VariableDiscount implements DiscountInterface {
+    private readonly _type: discountType = 'variable';
+    private readonly _value: number;
+
+    constructor(value: number = 0) {
         this._value = value;
 
-        if (this._type != 'none' && value <= 0) {
+        if (value <= 0) {
             throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
         }
     }
+
+    apply(price : number) : number {
+
+        return (price - (price * this._value / 100));
+    }
+
+    showCalculation(price : number) : string {
+
+        return price + " € -  "+ this._value +"%";
+    }
 }
+
+class FixedDiscount implements DiscountInterface {
+    private readonly _type: discountType = 'variable';
+    private readonly _value: number;
+
+    constructor(value: number = 0) {
+        this._value = value;
+
+        if (value <= 0) {
+            throw new Error('You cannot create a ' + this._type + ' discount with a negative value');
+        }
+    }
+
+    apply(price : number) : number {
+
+        return Math.max(0, price - this._value);
+    }
+
+    showCalculation(price : number) : string {
+
+        return price + "€ -  "+ this._value +"€ (min 0 €)";
+    }
+}
+
+class NoDiscount implements DiscountInterface {
+    private readonly _type: discountType = 'none';
+    private readonly _value: number;
+
+    constructor() {}
+
+    apply(price : number) : number {
+
+        return price;
+    }
+
+    showCalculation(price : number) : string {
+
+        return "No discount";
+    }
+}
+
+
+//previously existing code, adjusted for having 3 discount classes
+class Product {
+    private _name : string;
+    private _price : number;
+    private _discount : DiscountInterface;
+
+    constructor(name: string, price: number, discount: DiscountInterface) {
+        this._name = name;
+        this._price = price;
+        this._discount = discount;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get discount(): DiscountInterface {
+        return this._discount;
+    }
+
+    get originalPrice(): number {
+        return this._price;
+    }
+
+    //The reason we call this function "calculateX" instead of using a getter on Price is because names communicate a lot of meaning between programmers.
+    //most programmers would assume a getPrice() would be a simple display of a property that is already calculated, but in fact this function does a (more expensive) operation to calculate on the fly.
+    calculatePrice() : number {
+        return this._discount.apply(this._price);
+    }
+
+    showCalculation() : string {
+        return this._discount.showCalculation(this._price);
+    }
+}
+
+class shoppingBasket {
+    //this array only accepts Product objects, nothing else
+    private _products: Product[] = [];
+
+    get products(): Product[] {
+        return this._products;
+    }
+
+    addProduct(product: Product) {
+        this._products.push(product);
+    }
+}
+
+let cart = new shoppingBasket();
+cart.addProduct(new Product('Chair', 25, new FixedDiscount(10)));
+//cart.addProduct(new Product('Chair', 25, new Discount("fixed", -10)));
+cart.addProduct(new Product('Table', 50, new VariableDiscount( 25)));
+cart.addProduct(new Product('Bed', 100, new NoDiscount()));
+
+const tableElement = document.querySelector('#cart tbody');
+cart.products.forEach((product) => {
+    let tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerText = product.name;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.originalPrice.toFixed(2) + " €";
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.calculatePrice().toFixed(2) + " €";
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.showCalculation();
+    tr.appendChild(td);
+
+    tableElement.appendChild(tr);
+});
